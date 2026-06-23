842c7062d74ecfdb139a82e9179802336186c52c
Jan Larwig <jan@larwig.com>
2026-06-23 16:55:28 +0200

    fix: remove unnecessary animations
    Signed-off-by: Jan Larwig <jan@larwig.com>

--- src/internal/render/render.go
+++ src/internal/render/render.go
@@ -55,7 +55,7 @@ func renderLanguages(template string, summary stats.Summary) string {
 	var progress strings.Builder
 	var langList strings.Builder
 
-	for index, language := range summary.Languages {
+	for _, language := range summary.Languages {
 		color := language.Color
 		if color == "" {
 			color = "#000000"
@@ -66,12 +66,12 @@ func renderLanguages(template string, summary stats.Summary) string {
 			language.Percent,
 		))
 		langList.WriteString(fmt.Sprintf(`
-<li style="animation-delay: %dms;">
+<li>
 <svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:%s;" viewBox="0 0 16 16" version="1.1" width="16" height="16"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">%s</span>
 <span class="percent">%.2f%%</span>
 </li>
-`, index*150, html.EscapeString(color), html.EscapeString(language.Name), language.Percent))
+`, html.EscapeString(color), html.EscapeString(language.Name), language.Percent))
 	}
 
 	replacer := strings.NewReplacer(
